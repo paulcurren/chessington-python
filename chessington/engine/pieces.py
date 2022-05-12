5ef71111fd474e1ff5f123c97f09964fa6ab3610
Paul Curren <paul.curren@necsws.com>
2022-05-12 14:13:27 +0100

    red-1 fixed

--- chessington/engine/pieces.py
+++ chessington/engine/pieces.py
@@ -34,8 +34,22 @@ class Pawn(Piece):
     A class representing a chess pawn.
     """
 
+    isFirstMove = True
+
     def get_available_moves(self, board):
-        return []
+
+        square = board.find_piece(self)
+        
+        if (self.player == Player.WHITE):
+            if (self.isFirstMove):
+                return [Square.at(square.row + 1, square.col), Square.at(square.row + 2, square.col)]
+            else:
+                return [Square.at(square.row + 1, square.col)]
+
+        if (self.isFirstMove):
+            return [Square.at(square.row - 1, square.col), Square.at(square.row - 2, square.col)]
+        else:
+            return [Square.at(square.row - 1, square.col)]
 
 
 class Knight(Piece):
